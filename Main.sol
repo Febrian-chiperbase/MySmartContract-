d574ef5968517c1d0350783f8b28dff9c5f815a4
Febrian-chiperbase <fery.pradana@binus.ac.id>
2025-02-05 10:27:43 +0700

    Update Main.sol
    New function getDescription

--- Main.sol
+++ Main.sol
@@ -1,20 +1,5 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract Counter {
-    uint256 public count;
-    string public description;
-
-    constructor(string memory _description) {
-        count = 0;
-        description = _description;
-    }
-
-    function increment() public {
-        count += 1;
-    }
-
-    function getCount() public view returns (uint256) {
-        return count;
+ // New function to return the description
+    function getDescription() public view returns (string memory) {
+        return description;
     }
 }
